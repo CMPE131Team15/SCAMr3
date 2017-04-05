565c1c9f2ea699073ac703434bfca34a40edeefa
Awcmon <adcarbine@yahoo.com>
2017-04-04 18:37:33 -0700

    Added Students Model and Controller/View

---
+++ SCAMr3/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SCAMr3.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Major { get; set; }
+		public string Year { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		//NEED TO SERIALIZE/DESERIALIZE
+		public string UncompletedCourses { get; set; }
+		public string CurrentCourses { get; set; }
+		public string Friends { get; set; }
+		/*
+		public List<string> UncompletedCourses { get; set; }
+		public List<string> CurrentCourses { get; set; }
+		public List<int> Friends { get; set; }
+		*/
+	}
+
+	public class StudentDBContext : DbContext
+	{
+		public DbSet<Student> Students { get; set; }
+	}
+}
