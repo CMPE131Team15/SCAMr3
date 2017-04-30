abf163704c5e2fa909727d1d73dae1e084adce03
Awcmon <adcarbine@yahoo.com>
2017-04-29 21:17:28 -0700

    Added Course Model

---
+++ SCAMr3/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SCAMr3.Models
+{
+	public class Course
+	{
+		public int ID { get; set; }
+		public string Name { get; set; } //For instance, CMPE131
+		public string Title { get; set; } //For instance, "Software Engineering I"
+		public string Description { get; set; }
+	}
+
+	public class CourseDBContext : DbContext
+	{
+		public DbSet<Course> Courses { get; set; }
+	}
+}
