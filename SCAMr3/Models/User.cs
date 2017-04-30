9d074b5669ee5aafb42767b4d188aafbf1106ebe
Awcmon <adcarbine@yahoo.com>
2017-04-29 20:43:12 -0700

    Added User Model

---
+++ SCAMr3/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SCAMr3.Models
+{
+	public class User
+	{
+		public int ID { get; set; }
+		public string UserID { get; set; }
+		public string StudentID { get; set; }
+	}
+
+	public class UserDBContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+	}
+}
