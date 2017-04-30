d16973134cbe4da954d27df22702ec0249251485
Awcmon <adcarbine@yahoo.com>
2017-04-29 21:17:45 -0700

    Added Class Model

---
+++ SCAMr3/Models/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SCAMr3.Models
+{
+	public class Class
+	{
+		public int ID { get; set; }
+		public string ClassNumber { get; set; } //The unique class identifier number
+		public int CourseID { get; set; } //ID of the course from the course model this class is
+		public int SectionNumber { get; set; } 
+		public string Instructor { get; set; }
+		public string Days { get; set; }
+		public string StartTime { get; set; }
+		public string EndTime { get; set; }
+	}
+
+	public class ClassDBContext : DbContext
+	{
+		public DbSet<Class> Class { get; set; }
+	}
+}
