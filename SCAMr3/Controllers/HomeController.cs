742a14adb95da24ea80ef319181915d193e49bd6
Awcmon <adcarbine@yahoo.com>
2017-05-09 23:33:39 -0700

    Integrated Khoa's views (view4)

--- SCAMr3/Controllers/HomeController.cs
+++ SCAMr3/Controllers/HomeController.cs
@@ -26,5 +26,45 @@ namespace SCAMr3.Controllers
 
 			return View();
 		}
+
+		public ActionResult Boards()
+		{
+			ViewBag.Message = "The Discussion board page.";
+
+			return View();
+		}
+
+		public ActionResult Schedule()
+		{
+			ViewBag.Message = "My Schedule page.";
+
+			return View();
+		}
+
+		public ActionResult Profile()
+		{
+			ViewBag.Message = "My Profile page.";
+
+			return View();
+		}
+
+		public ActionResult FAQs()
+		{
+			ViewBag.Message = "FAQs";
+
+			return View();
+		}
+
+		public ActionResult Threads()
+		{
+			return View();
+		}
+
+		public ActionResult GeneratedSchedule()
+		{
+			ViewBag.Message = "Generated Schedule page.";
+
+			return View();
+		}
 	}
 }
